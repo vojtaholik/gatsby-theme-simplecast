0e3c75ce874388514c47643e0a7f6e1bc07eccee
Vojta Holik <vojta@egghead.io>
2019-08-13 16:28:59 +0200

    fix bg color

--- demo/src/gatsby-plugin-theme-ui/index.js
+++ demo/src/gatsby-plugin-theme-ui/index.js
@@ -3,7 +3,7 @@ export default {
   initialColorMode: 'dark',
   colors: {
     text: 'rgba(255, 255, 255, 0.9)',
-    background: 'red',
+    background: '#1A2232',
     backgroundLighten10: '#232B3B',
     backgroundLighten20: '#2C3648',
     primaryDarken: '#7A5EFF',
